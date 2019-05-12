206fc1ce73ca6e086f9cba99b05a7b59b27d25d6
Ian <spyguyian@gmail.com>
2019-05-12 18:45:48 -0400

    ready for raspberry pi testing

--- src/ian_bot/scripts/rpi_pwm.py
+++ src/ian_bot/scripts/rpi_pwm.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python
 
+
 import rospy
-from std_msgs.msg import Float32MultiArray
+from ian_bot.msg import PWM as PWMmsg
+from ian_bot.srv import PWMResponse, PWM as PWMsrv
 import pigpio
 
 class RPiPWM():
 
 	def __init__(self):
-		self.sub = rospy.Subscriber('', Float32MultiArray, callback)
+		self.sub = rospy.Subscriber('/pi/pwm', PWMmsg, set_PWM_callback)
+		self.srv = rospy.Service('/pi/pwm_duty', PWMsrv, handle_get_PWM)
 		self.gpio = pigpio.pi()
 		if not pi.connected:
-			rospy.logerr("pigpio not connected")
+			rospy.logerr("pigpio not connected")###########start it
 			exit()
 		
 		
-
-
-	def callback(self, data):
-		#data.dim
-		for pwm in data.data:
-			
-
+	def handle_get_PWM(self, req):
+		return PWMResponse(gpio.pi.get_PWM_dutycycle(req.pin))
+		
+		
+	def set_PWM_callback(self, data):
+		#data.pin, data.duty
+		if 0>data.pin>31 or 0>data.duty>255:
+			rospy.logerr_throttle(1, "Malformed PWM message: " + data)
+		self.gpio.set_PWM_dutycycle(data.pin, data.duty)
 
 if __name__ == "__main__":
 	try:
