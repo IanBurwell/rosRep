c0675f2b9323ea1ce08281f9b59fb0795f980f7a
Ian <spyguyian@gmail.com>
2019-05-19 23:20:08 +0000

    pi pwm working

--- src/ian_bot/src/ian_bot/rpi_pwm.py
+++ src/ian_bot/src/ian_bot/rpi_pwm.py
@@ -2,35 +2,35 @@
 
 
 import rospy
-from ian_bot.msg import PWM as PWMmsg
-from ian_bot.srv import PWMResponse, PWM as PWMsrv
+from ian_bot.msg import PWM
+from ian_bot.srv import PWMgetResponse, PWMget
 import pigpio
 
 class RPiPWM():
 
 	def __init__(self):
-		self.sub = rospy.Subscriber('/pi/pwm', PWMmsg, set_PWM_callback)
-		self.srv = rospy.Service('/pi/pwm_duty', PWMsrv, handle_get_PWM)
+		self.sub = rospy.Subscriber("/pi/pwm", PWM, self.set_PWM_callback)
+		self.srv = rospy.Service('/pi/pwm_duty', PWMget, self.handle_get_PWM)
 		self.gpio = pigpio.pi()
-		if not pi.connected:
-			rospy.logerr("pigpio not connected")###########start it
+		if not self.gpio.connected:
+			rospy.logerr("pigpio not connected") ##########start it
 			exit()
-		
+		rospy.loginfo("initialized")
 		
 	def handle_get_PWM(self, req):
-		return PWMResponse(gpio.pi.get_PWM_dutycycle(req.pin))
+		return PWMgetResponse(self.gpio.get_PWM_dutycycle(req.pin))
 		
-		
-	def set_PWM_callback(self, data):
+	def set_PWM_callback(self, msg):
 		#data.pin, data.duty
-		if 0>data.pin>31 or 0>data.duty>255:
-			rospy.logerr_throttle(1, "Malformed PWM message: " + data)
-		self.gpio.set_PWM_dutycycle(data.pin, data.duty)
+		if not 0<=msg.pin<=31 or not 0<=msg.duty<=255:
+			rospy.logerr_throttle(1, "Malformed PWM message: " + msg)
+		rospy.loginfo("Pin: %d, Duty: %d", msg.pin, msg.duty/255.0*100)
+		self.gpio.set_PWM_dutycycle(msg.pin, msg.duty)
 
 if __name__ == "__main__":
 	try:
 		rospy.init_node('rpi_pwm', anonymous=True)
 		RPiPWM()
 		rospy.spin()
-	except rospy.RoSInterrupException:
+	except rospy.ROSInterruptException:
 		pass
